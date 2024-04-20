d3cc82b392da38b1b0cf48f6aa7d3087c5b5aac0
Alex <buggyal.dev@gmail.com>
2024-04-20 10:43:00 -0500

    yay I got array working

--- src/main/java/me/buggyal/Main.java
+++ src/main/java/me/buggyal/Main.java
@@ -1,9 +1,29 @@
 package me.buggyal;
 
+import org.json.JSONArray;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            parseExercises();
+        } catch (URISyntaxException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void parseExercises() throws URISyntaxException, IOException {
+        Path path = Paths.get(Main.class.getResource("/exercises.json").toURI());
+        String jsonContent = new String(Files.readAllBytes(path), StandardCharsets.UTF_8);
+        JSONArray exerciseArray = new JSONArray(jsonContent);
+        System.out.println(exerciseArray.toString(2));
     }
 
 }
